6cc68500727c8f80bf5580e3f0edf5cc487ab20d
ryu <munam4@naver.com>
2021-06-30 19:42:52 +0900

    fail test1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
         
-        <p>
           hi!!! i'm 현선
         </p>
         <a
